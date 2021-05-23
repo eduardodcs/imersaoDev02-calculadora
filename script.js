ec7bd1e4322b29008a071737cdc4373a1cc54c2a
eduardodcs <eddcesilva@gmail.com>
2021-05-23 16:24:09 -0300

    Melhorias no JavaScript usando Dom

--- script.js
+++ script.js
@@ -1,25 +1,89 @@
-var primeiroValor = parseInt(prompt("Digite o primeiro valor: "))
+var visor = document.getElementById("calculadora_visor")
+var valorTexto = ""
+var valorTemp = ""
+var valor1
+var valor2
+var operador
+var valorResultado
 
-var segundoValor = parseInt(prompt("Digite o segundo valor: "))
-
-var operacao = prompt("Para divisão digite 1. Para multiplicação digite 2. Para soma digite 3. Para subtração digite 4.")
-
-
-
-if (operacao == 1) {
-  var resultado = primeiroValor / segundoValor
-  document.write("<h2>" + primeiroValor + " / " + segundoValor + " = " + resultado + "</h2>")
-} else if (operacao == 2) {
-  var resultado = primeiroValor * segundoValor
-  document.write("<h2>" + primeiroValor + " x " + segundoValor + " = " + resultado + "</h2>")
-} else if (operacao == 3) {
-  var resultado = primeiroValor + segundoValor
-  document.write("<h2>" + primeiroValor + " + " + segundoValor + " = " + resultado + "</h2>")
-} else if (operacao == 4) {
-  var resultado = primeiroValor - segundoValor
-  document.write("<h2>" + primeiroValor + " - " + segundoValor + " = " + resultado + "</h2>")
-} else {
-  document.write("<h2>Opção inválida</h2>")
+function zerarValores() {
+  valorTexto = ""
+  valorTemp = ""
+  valor1 = null
+  valor2 = null
+  operador = null
+  valorResultado = null
+  visor.innerHTML = "<p></p>"
 }
 
 
+function main(e) {
+  if (e == "=") {
+    if (valorTexto != ""){
+      if (verificaPrimeiroValorNull()){
+        valor1 = parseInt(valorTemp)
+        valorResultado = parseInt(valorTemp)
+        console.log(valorResultado)
+      } else {
+        valor2 = parseInt(valorTemp)
+        calcular()
+        exibirResultado()
+        valorTexto = valorResultado
+      }
+    }
+
+  } else if (ehOperador(e)) {
+    if (valorTexto != ""){
+      if (verificaPrimeiroValorNull()){
+        valor1 = parseInt(valorTemp)
+        valorTemp = ""
+        exibirValorNoVisor(e)
+        operador = e
+      } else {
+        valor2 = parseInt(valorTemp)
+        calcular()
+        valor2 = ""
+        valorTemp = ""
+        valor1 = valorResultado
+        operador = e
+        exibirValorNoVisor(e)
+        console.log(valorResultado)
+      }
+    }
+  } else {
+    exibirValorNoVisor(e)
+    valorTemp = valorTemp + e
+  }
+}
+
+function verificaPrimeiroValorNull(){
+  if (valor1 == null){
+    return true
+  }
+  return false
+}
+
+function calcular(){
+  switch (operador) {
+    case "+": {valorResultado = valor1 + valor2; break}
+    case "-": {valorResultado = valor1 - valor2; break}
+    case "x": {valorResultado = valor1 * valor2; break}
+    case "/": {valorResultado = valor1 / valor2; break}
+  }
+}
+
+function exibirValorNoVisor(e) {
+  valorTexto = valorTexto + e
+  visor.innerHTML = "<p>" + valorTexto + "</p>" 
+}
+
+function exibirResultado() {
+  visor.innerHTML = "<p>" + valorResultado + "</p>" 
+}
+
+function ehOperador(e){
+  if(e == "+" || e == "-" || e == "x" || e == "/"){
+    return true
+  }
+  return false
+}
